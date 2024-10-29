99d0fcea3333b00e1ce08411f3d00a7d81263b7b
Artur8090 <artur.cheuchiev@gmail.com>
2024-10-29 17:18:06 +0100

    node modules

---
+++ P2nodeModules/server.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+/*
+let fileContent = fs.readFileSync('text.txt','utf-8')
+console.log(fileContent)
+
+fs.appendFileSync('text.txt','Node.js представляет среду выполнения кода на JavaScript, которая построена на основе движка JavaScript Chrome V8, который позволяет транслировать вызовы на языке JavaScript в машинный код. Node.js прежде всего предназначен для создания серверных приложений на языке JavaScript.Для создания приложений можно использовать практически все стандартные конструкции языка JavaScript. Исключением является работа с DOM, так как приложение будет запускаться на сервере, а не в браузере, поэтому DOM и такие объекты как window или document в данном случае нам будут недоступны.')
+let content = fs.readFileSync('text.txt','utf-8')
+console.log(content)
+
+fs.writeFileSync('hello.txt','hello world')
+
+fs.readdir('../nodeIntro','utf-8', 
+    function(error, data){
+        console.log(data)
+    }
+);
+fs.unlinkSync('hello.txt')
+*/
