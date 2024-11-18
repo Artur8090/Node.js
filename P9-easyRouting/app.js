98ce9eb7c4d8ff933dcddf1625cfe71875fb51a6
Artur8090 <artur.cheuchiev@gmail.com>
2024-11-18 22:41:37 +0100

    homework node js

--- P9-easyRouting/app.js
+++ P9-easyRouting/app.js
@@ -1,7 +1,22 @@
 const http = require('http');
-http.createServer(function(request, response){
-    if(request.url === '/home' || request.url ==='/'){
-        response.setHeader('Content-Type', 'text/html', 'charset=utf-8;');
-        
+
+http.createServer((req, res) => {
+    // Set the default content type to UTF-8
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+    if (req.url === '/page1/') {
+        res.statusCode = 200; // Set status code to 200
+        res.end('первая страница');
+    } else if (req.url === '/page2/') {
+        res.statusCode = 200; // Set status code to 200
+        res.end('вторая страница');
+    } else if (req.url === '/page3/') {
+        res.statusCode = 200; // Set status code to 200
+        res.end('третья страница');
+    } else {
+        res.statusCode = 404; // Set status code to 404
+        res.end('страница не найдена');
     }
-}).listen(3000)
+}).listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
